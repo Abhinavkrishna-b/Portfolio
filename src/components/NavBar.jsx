254999fac38eeb0823fc1075d647189fb627794a
Abhinavkrishna-b <abhinavkrishna.b.2005@gmail.com>
2025-12-09 11:30:36 +0530

    Animation-1 added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,6 +16,36 @@ const NavBar = () =>{
 
     const [open,setOpen]=useState(false)
 
+    const topVariants ={
+        closed:{
+            rotate:0
+        },
+        opened:{
+            rotate:45,
+            backgroundColor: "rgb(255,255,255)"
+        }
+    }
+
+    const midVariants={
+        closed:{
+            opacity:1
+        },
+        opened:{
+            opacity:0
+        }
+    }
+
+    const bottomVariants={
+        closed:{
+            rotate:0
+        },
+        opened:{
+            rotate:-45,
+            backgroundColor:"rgb(255,255,255)"
+        }
+    }
+
+
     return(
         <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-16 xl:px-28 text-xl'>
             
@@ -52,12 +82,12 @@ const NavBar = () =>{
             <div className='md:hidden'>
                 <button className="w-10 h-8 flex flex-col justify-between z-50 relative"
                  onClick={() => setOpen((prev) => (!prev))}>
-                    <div className='w-10 h-1 bg-black rounded'></div>
-                    <div className='w-10 h-1 bg-black rounded'></div>
-                    <div className='w-10 h-1 bg-black rounded'></div>
+                    <motion.div variants={topVariants} animate={open ? "opened" : "closed"} className='w-10 h-1 bg-black rounded origin-left'></motion.div>
+                    <motion.div variants={midVariants} animate={open ? "opened" : "closed"} className='w-10 h-1 bg-black rounded'></motion.div>
+                    <motion.div variants={bottomVariants} animate={open ? "opened" : "closed"} className='w-10 h-1 bg-black rounded origin-left'></motion.div>
                 </button>
                 { open && (
-                <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+                <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40">
                     {links.map(link=>(
                         <Link href={link.url} key={link.title}>{link.title}</Link>
                     ))}
